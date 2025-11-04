d70e02ff12ec5dfc88f6e5877b83a2cf0c058a3d
davidbaarbosa <davidbaarbosa18@gmail.com>
2025-11-03 22:40:08 -0300

    Atualizações

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -13,10 +13,10 @@ function Footer() {
                     </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <a href="https://www.instagram.com/seu_usuario" target="_blank">
+                        <a href="https://www.instagram.com/" target="_blank">
                             <InstagramLogoIcon size={48} weight='bold' />
                         </a>
-                        <a href="https://www.facebook.com/seu_usuario" target="_blank">
+                        <a href="https://www.facebook.com/" target="_blank">
                             <FacebookLogoIcon size={48} weight='bold' />
                         </a>
                     </div>
